6d65ef2b45fe838f974a46fbd28075d8fb0e006e
dyedysJuina <82832948+dyedysJuina@users.noreply.github.com>
2023-06-20 21:55:21 -0400

    Update cad_produto.php

--- cad_produto.php
+++ cad_produto.php
@@ -10,6 +10,29 @@
   <div class="container">
     <form method="POST" action="">
       <div class="row">
+          
+          
+          <div class="col-md-4">
+          <label for="empresas">Supermercados</label>
+          <select id="txt-id-empresa" class="form-control" name="txt-id-empresa">
+            <option value="">Selecione Supermercado</option>
+            <?php
+            include_once("conexao.php");
+            $sql = "SELECT * FROM empresas";
+            $query = mysqli_query($conn, $sql);
+            while($resultado = mysqli_fetch_assoc($query)) {
+              ?>
+              <option value="<?php echo $resultado['idempresa']; ?>"><?php echo $resultado['nome']; ?></option>
+              <?php
+            }
+            ?>
+          </select>
+        </div>
+          
+          
+          
+          
+          
         <div class="col-md-4">
           <label for="estadoSelect">Categoria Principal</label>
           <select id="categoriaprincipal" class="form-control" name="categoriaprincipal">
@@ -42,9 +65,62 @@
         </div>
         
         
+        <div class="col-md-4">
+          <label for="cidadeSelect">Nome do Produto</label>
+            <input type="text" placeholder="Nome do Produto" name="txt-nome-produto">
+        </div>
         
+        <div class="col-md-4">
+          <label for="cidadeSelect">Descrição do Item</label>
+            <input type="text" placeholder="Descreva sobre o item" name="txt-descricao">
+        </div>
         
+        <div class="col-md-4">
+          <label for="cidadeSelect">Preco de Compra</label>
+            <input type="text" placeholder="R$" name="txt-preco-compra">
+        </div>
         
+        <div class="col-md-4">
+          <label for="cidadeSelect">Preco de Venda</label>
+            <input type="text" placeholder="R$" name="txt-preco-venda">
+        </div>
+        
+        <div class="col-md-4">
+          <label for="cidadeSelect">qtd comprada</label>
+            <input type="number" placeholder="Qtd-comprada" name="qtd-comprada">
+        </div>
+        
+       <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST['txt-nome-produto'];
+    $subcategoria_id = $_POST['categoria'];
+    $descricao = $_POST['txt-descricao'];
+
+    // Obter os preços de compra e venda substituindo vírgula por ponto
+    $preco_compra = str_replace(',', '.', $_POST['txt-preco-compra']);
+    $preco_venda = str_replace(',', '.', $_POST['txt-preco-venda']);
+
+    $empresa_id = $_POST['txt-id-empresa'];
+    $data_cadastro = date('Y-m-d H:i:s');
+    $qtd_comprada = $_POST['qtd-comprada'];
+
+    // Calcular o total da compra
+    $total_da_compra = $preco_compra * $qtd_comprada;
+
+    $sql = "INSERT INTO produtos (nome, subcategoria_id, descricao, preco_compra, preco_venda, idsupermercado, data_cadastro, qtd_comprada, total_da_compra) values 
+    ('$nome','$subcategoria_id','$descricao','$preco_compra','$preco_venda','$empresa_id','$data_cadastro','$qtd_comprada','$total_da_compra')";
+    $query = mysqli_query($conn, $sql);
+    $linha = mysqli_affected_rows($conn);
+        
+        if($linha === 1){
+            echo"cadastrou com sucesso";
+        }else{
+            echo"nao cadastrou";
+        }
+        }
+        ?>
+
+        <button type="submit">Cadastrar Item</button>
         
         
       </div>
