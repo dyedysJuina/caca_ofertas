7e2e3d3ca86cc86496d2fe46af90cf53cf5604a4
dyedysJuina <82832948+dyedysJuina@users.noreply.github.com>
2023-06-25 13:46:04 -0400

    Update listar_todos_produtos.php

--- listar_todos_produtos.php
+++ listar_todos_produtos.php
@@ -1,40 +1,100 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
+  <style>
+    body{
+      background-color: #f9f9f9;
+    }
+    .valor input{
+      width:50px;
+      font-size:12px;
+    }
 
-<?php include_once("conexao.php");
+    input{
+      font-size:12px;
+    }
+    #cabecalho{
+      background-color: #666666;
+      color:white;
+    }
+    </style>
 
-$sql = "SELECT * FROM produtos";
-$query = mysqli_query($conn, $sql);
 
-while($resultado = mysqli_fetch_assoc($query)) {
-    $idproduto = $resultado['idproduto'];
-    $nome = $resultado['nome'];
-    $idempresa = $resultado['idempresa'];
-    $preco_unitario = $resultado['preco_unitario'];
-    $preco_venda = $resultado['preco_venda'];
+<div class="container-fluid">
+  <h2 class="text-dark">Cadastrar Oferta</h2>
+  <p>Bem vindo ao supermercado Pasqualotto</p>
+
+<form action="" method="get">
+
+
+  <div class="table">
+    <table class="table table-bordered ">
+      <thead>
+        <tr>
+          <th id="cabecalho">PRODUTO</th>
+          <th id="cabecalho">UNID</th>
+          <th id="cabecalho">VENDA</th>
+          <th id="cabecalho">OFERTA</th>
+          <th id="cabecalho">DESC %</th>
+          <th id="cabecalho">DESC R$</th>
+          <th id="cabecalho">INICIO</th>
+          <th id="cabecalho">FIM</th>
+          <th id="cabecalho">INSERIR</th>
+        
+          
+        </tr>
+      </thead>
+      <tbody>
+    <?php
+    if(isset($_GET['idempresa'])){
+      $pegaid = $_GET['idempresa'];
+    
+    $sql = "SELECT * FROM produtos where idempresa = $pegaid";
+    include_once("conexao.php");
+    $query = mysqli_query($conn, $sql);
+    while ($resultado = mysqli_fetch_assoc($query)) {
+      $idempresa = $resultado['idempresa'];
+      $id_produto = $resultado['idproduto'];
+      $nome = $resultado['nome'];
+      $preco_unitario = $resultado['preco_unitario'];
+      $preco_venda = $resultado['preco_venda'];
 ?>
-    <form action="processa_oferta.php" method="GET">
-        <input type="hidden" name="idproduto[]" value="<?php echo $idproduto; ?>">
-        <input type="hidden" name="idempresa[]" value="<?php echo $idempresa; ?>">
-        <input type="text" placeholder="Nome produto" value="<?php echo $nome; ?>" name="nome[]">
-        <input type="text" placeholder="Preço unitário" value="<?php echo $preco_unitario; ?>" name="preco_unitario[]">
-        <input type="text" placeholder="Preço venda" value="<?php echo $preco_venda; ?>" name="preco_venda[]">
-        <input type="text" placeholder="Preço da oferta" value="" name="preco_da_oferta[]">
-        <input type="date" placeholder="Início da Oferta" value="" name="inicio_oferta[]">
-        <input type="date" placeholder="Fim da Oferta" value="" name="fim_oferta[]">
-        <button type="submit">Cadastrar</button>
-        <hr>
-    </form>
-<?php
-}
-?>
+
+<form action="processa_oferta.php" method="GET">
+
+  <tr>
+    <input type="hidden" placeholder="idempresa" value="<?php echo $idempresa; ?>" name="idempresa" readonly>
+    <input type="hidden" placeholder="idproduto" value="<?php echo $id_produto; ?>" name="idproduto" readonly>
+    <td class="input-nome"><input type="text" placeholder="nome produto" value="<?php echo $nome; ?>" name="nome" readonly></td>
+    <td class="valor"><input type="text" placeholder="R$ unit" value="<?php echo $preco_unitario; ?>" name="preco_unitario" readonly></td>
+    <td class="valor"><input type="text" placeholder="preco  venda" value="<?php echo $preco_venda; ?>" name="preco_venda" readonly></td>
+    <td class="valor"> <input type="text" placeholder="R$ " value="" name="preco_oferta"></td>
+    <td class="valor" ><input type="text" placeholder="Inicio" value="" name="porcentagem_desconto"></td>
+    <td class="valor"> <input type="text" placeholder="R$ " value="" name="valor_atualizado"></td>
+    <td><input type="date" placeholder="Inicio" value="" name="inicio_oferta"></td>
+    <td><input type="date" placeholder="Fim " value="" name="fim_oferta"></td>
+    
+    <td><button type="submit" class="btn btn-primary">Ofertar</button></td>
+  </tr>
+</form>
+<?php } } ?>
+
+</tbody>
+    </table>
+  </div>
+</div>
+<script>
+  $(document).ready(function () {
+    $('#example').DataTable();
+});
+ </script>
 
 </body>
 </html>
