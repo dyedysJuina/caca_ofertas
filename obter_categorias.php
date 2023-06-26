c960eff7fe0debaca0a7387e49970655e766abd7
dyedysJuina <82832948+dyedysJuina@users.noreply.github.com>
2023-06-26 14:02:22 -0400

    Update and rename get-categorias.php to obter_categorias.php

---
+++ obter_categorias.php
@@ -0,0 +1,22 @@
+<?php
+// Obter a conexão com o banco de dados
+include_once("conexao.php");
+
+// Verificar se o ID da categoria principal foi enviado via POST
+if (isset($_POST['idcatprincipal'])) {
+    $idcatprincipal = $_POST['idcatprincipal'];
+
+    // Consultar as categorias com base no ID da categoria principal
+    $sql = "SELECT * FROM categorias WHERE idcatprincipal = '$idcatprincipal'";
+    $query = mysqli_query($conn, $sql);
+
+    // Construir as opções do select das categorias
+    $options = "<option value=''>Categoria</option>";
+    while ($resultado = mysqli_fetch_assoc($query)) {
+        $options .= "<option value='".$resultado['idcategoria']."'>".$resultado['nome']."</option>";
+    }
+
+    // Retornar as opções como resposta para o JavaScript
+    echo $options;
+}
+?>
