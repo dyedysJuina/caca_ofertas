8a5d1fe016a18e937082dc7dff6bb5f96d673819
dyedysJuina <82832948+dyedysJuina@users.noreply.github.com>
2023-06-26 14:01:43 -0400

    Update cad_produto.php

--- cad_produto.php
+++ cad_produto.php
@@ -1,225 +1,89 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Exemplo de Selects com Bootstrap e AJAX</title>
-  <!-- Importando bibliotecas -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <title>Exemplo de Seleções e Inputs Lado a Lado</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
 <body>
-  <div class="container">
-    <form method="POST" action="" enctype="multipart/form-data">
-      <div class="row">
-        <div class="col-md-4">
-          <label for="empresas">Supermercados</label>
-          <select id="txt-id-empresa" class="form-control" name="txt-id-empresa">
-            <option value="">Selecione Supermercado</option>
-            <?php
-            include_once("conexao.php");
-            $sql = "SELECT * FROM empresas";
-            $query = mysqli_query($conn, $sql);
-            while($resultado = mysqli_fetch_assoc($query)) {
-              ?>
-              <option value="<?php echo $resultado['idempresa']; ?>"><?php echo $resultado['nome']; ?></option>
-              <?php
-            }
-            ?>
-          </select>
+    <?php include_once("conexao.php");?>
+    <div class="container">
+        <h2>Exemplo de Seleções e Inputs Lado a Lado</h2>
+        <form action="" method="post">
+            <div class="row">
+                <div class="col">
+                    <select class="form-control" name="idcatprincipal" id="idcatprincipal">
+                        <option>Tip de Oferta</option>
+                        <?php
+                        // Carrega as opções da categoria principal do banco de dados
+                        $sql = "SELECT * FROM categoria_principal";
+                        $query = mysqli_query($conn, $sql);
+                        while($resultado = mysqli_fetch_assoc($query)){
+                            ?>
+                            <option value="<?php echo $resultado['idcatprincipal'];?>"><?php echo $resultado['nome'];?></option>
+                            <?php
+                        }
+                        ?>
+                    </select>
+                </div>
+                <div class="col">
+                    <select class="form-control" name="idcategoria" id="idcategoria">
+                        <option value="">Categoria</option>
+                    </select>
+                </div>
+                <div class="col">
+                    <select class="form-control" name="idsubcategoria" id="idsubcategoria">
+                        <option value="">Subcategoria</option>
+                    </select>
+                </div>
+            </div>
+        </form>
+        <br>
+        <div class="row">
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Input 1">
+            </div>
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Input 2">
+            </div>
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Input 3">
+            </div>
         </div>
-        
-        <div class="col-md-4">
-          <label for="estadoSelect">Categoria Principal</label>
-          <select id="categoriaprincipal" class="form-control" name="categoriaprincipal">
-            <option value="">Selecione Categoria Principal</option>
-            <?php
-            include_once("conexao.php");
-            $sql = "SELECT * FROM categoria_principal";
-            $query = mysqli_query($conn, $sql);
-            while($resultado = mysqli_fetch_assoc($query)) {
-              ?>
-              <option value="<?php echo $resultado['idcatprincipal']; ?>"><?php echo $resultado['nome']; ?></option>
-              <?php
-            }
-            ?>
-          </select>
-        </div>
-        
-        <div class="col-md-4">
-          <label for="cidadeSelect">Categorias</label>
-          <select id="categoria" class="form-control" name="categoria">
-            <option value="">Selecione Categoria</option>
-          </select>
-        </div>
-        
-        <div class="col-md-4">
-          <label for="cidadeSelect">Subcategorias</label>
-          <select id="subcategoria" class="form-control" name="subcategoria">
-            <option value="">Selecione Subcategoria</option>
-          </select>
-        </div>
-        
-        <div class="col-md-4">
-          <label for="cidadeSelect">Nome do Produto</label>
-          <input type="text" class="form-control" placeholder="Nome do Produto" name="txt-nome-produto">
-        </div>
-        
-        <div class="col-md-4">
-          <label for="cidadeSelect">Descrição do Item</label>
-          <input type="text" class="form-control" placeholder="Descreva sobre o item" name="txt-descricao">
-        </div>
-        
-        <div class="col-md-4">
-          <label for="cidadeSelect">Preco de Compra</label>
-          <input type="text" class="form-control" placeholder="R$" name="txt-preco-compra">
-        </div>
-        
-        <div class="col-md-4">
-          <label for="cidadeSelect">Preco de Venda</label>
-          <input type="text" class="form-control" placeholder="R$" name="txt-preço-venda">
-</div>
-
-    <div class="col-md-4">
-      <label for="cidadeSelect">Qtd Comprada</label>
-      <input type="number" class="form-control" placeholder="Qtd-comprada" name="qtd-comprada">
     </div>
-    
-    <div class="col-md-4">
-      <label for="cidadeSelect">Fotos do Produto</label>
-      <input type="file" class="form-control-file" name="imagens[]" multiple>
-    </div>
-    
-    <div class="col-md-12 mt-4">
-      <button type="submit" class="btn btn-primary">Cadastrar</button>
-    </div>
-  </div>
-</form>
-</div>
-<?php
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    include_once("conexao.php");
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#idcatprincipal').on('change', function() {
+                let idcatprincipal = this.value;
+                $.ajax({
+                    url: "obter_categorias.php", // Arquivo PHP para carregar as categorias
+                    type: "POST",
+                    data: { idcatprincipal: idcatprincipal },
+                    cache: false,
+                    success: function(result) {
+                        $('#idcategoria').html(result); // Insere as opções de categoria retornadas no select
+                    }
+                });
+            });
+        });
 
-    $nome = $_POST['txt-nome-produto'];
-    $subcategoria_id = $_POST['categoria'];
-    $descricao = $_POST['txt-descricao'];
-    $preco_compra = str_replace(',', '.', $_POST['txt-preco-compra']);
-    $preco_venda = str_replace(',', '.', $_POST['txt-preco-venda']);
-    $empresa_id = $_POST['txt-id-empresa'];
-    $data_cadastro = date('Y-m-d H:i:s');
-    $qtd_comprada = $_POST['qtd-comprada'];
 
-    // Calcular o total da compra
-    $total_da_compra = $preco_compra * $qtd_comprada;
-
-    $sql = "INSERT INTO produtos (nome, subcategoria_id, descricao, preco_compra, preco_venda, idsupermercado, data_cadastro, qtd_comprada, total_da_compra) VALUES 
-    ('$nome','$subcategoria_id','$descricao','$preco_compra','$preco_venda','$empresa_id','$data_cadastro','$qtd_comprada','$total_da_compra')";
-    $query = mysqli_query($conn, $sql);
-    $linha = mysqli_affected_rows($conn);
-    $ultimo_id = mysqli_insert_id($conn);
-
-    if ($linha === 1) {
-        $imagens = $_FILES['imagens'];
-        $numImagens = count($imagens['name']);
-
-        // Check the number of uploaded images
-        if ($numImagens < 1 || $numImagens > 8) {
-           echo '<div class="alert alert-danger" role="alert">Você deve enviar de 1 a 8 imagens!</div>';
-            exit;
-        }
-
-        for ($i = 0; $i < $numImagens; $i++) {
-            $nome_imagem = uniqid() . '_' . $imagens['name'][$i];
-            $url_caminho = "img_imagens/" . $nome_imagem;
-
-            // Faz o upload da imagem para a pasta "img_imagens"
-            $destino = "img_imagens/" . $nome_imagem;
-            $arquivo_temporario = $imagens['tmp_name'][$i];
-            
-            if (move_uploaded_file($arquivo_temporario, $destino)) {
-                $sqlimg = "INSERT INTO img_produtos (nome, url_caminho, idproduto) VALUES ('$nome_imagem','$url_caminho','$ultimo_id')";
-                $queryimg = mysqli_query($conn, $sqlimg);
-                $linhaimg = mysqli_affected_rows($conn);
-
-                if ($linhaimg !== 1) {
-                    echo '<div class="alert alert-success" role="alert">Erro ao cadastrar imagem!</div>';
-                }
-            } else {
-                echo '<div class="alert alert-success" role="alert">Erro ao fazer upload!</div>';
-            }
-        }
-
-        echo '<div class="alert alert-success" role="alert">Cadastrado com sucesso!</div>';
-    } else {
-         echo '<div class="alert alert-danger" role="alert">Erro ao cadastrar o produto!</div>';
-    }
-}
-?>
-
-<script>
-  $(document).ready(function() {
-    $('#categoriaprincipal').on('change', function() {
-let idcatprincipal = this.value;
-$.ajax({
-url: "get-categorias.php",
-type: "POST",
-data: { idcatprincipal: idcatprincipal },
-cache: false,
-success: function(result) {
-$('#categoria').html(result);
-}
-});
-});
-
-$('#categoria').on('change', function() {
-  let categoria = this.value;
-  $.ajax({
-    url: "get-subcategorias.php",
-    type: "POST",
-    data: { categoria: categoria },
-    cache: false,
-    success: function(result) {
-      $('#subcategoria').html(result);
-    }
-  });
-});
-
-});
-</script>
-
+        $(document).ready(function() {
+            $('#idcategoria').on('change', function() {
+                let idcategoria = this.value;
+                $.ajax({
+                    url: "obter_subcategoria.php", // Arquivo PHP para carregar as categorias
+                    type: "POST",
+                    data: { idcategoria: idcategoria },
+                    cache: false,
+                    success: function(result) {
+                        $('#idsubcategoria').html(result); // Insere as opções de categoria retornadas no select
+                    }
+                });
+            });
+        });
+    </script>
 </body>
 </html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
