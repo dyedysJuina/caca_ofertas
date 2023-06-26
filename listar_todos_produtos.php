6c2f6e51e73482eddb63e113b3bfc2759619de7b
dyedysJuina <82832948+dyedysJuina@users.noreply.github.com>
2023-06-25 21:06:40 -0400

    Update listar_todos_produtos.php

--- listar_todos_produtos.php
+++ listar_todos_produtos.php
@@ -4,59 +4,62 @@
   <title>Bootstrap Example</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/2.0.1/js/dataTables.buttons.min.js"></script>
+  <script src="https://cdn.datatables.net/select/1.3.3/js/dataTables.select.min.js"></script>
+  <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-  <style>
-    body{
-      background-color: #f9f9f9;
-    }
-    .valor input{
-      width:50px;
-      font-size:12px;
-    }
 
-    input{
-      font-size:12px;
-    }
-    #cabecalho{
-      background-color: #666666;
-      color:white;
-    }
-    </style>
+<style>
+  html {
+    font-size: 80%;
+  }
 
+  .ids {
+    width: 70px;
+  }
 
-<div class="container-fluid">
-  <h2 class="text-dark">Cadastrar Oferta</h2>
-  <p>Bem vindo ao supermercado Pasqualotto</p>
+  .ids input {
+    width: 100%;
+    text-align: center;
+  }
 
-<form action="" method="get">
+  .input_nome {
+    background-color: blue;
+    min-width: 100%;
+    position: relative;
+  }
 
+  .input_nome input {
+    min-width: 100%;
+  }
+</style>
 
-  <div class="table">
-    <table class="table table-bordered ">
-      <thead>
-        <tr>
-          <th id="cabecalho">PRODUTO</th>
-          <th id="cabecalho">UNID</th>
-          <th id="cabecalho">VENDA</th>
-          <th id="cabecalho">OFERTA</th>
-          <th id="cabecalho">DESC %</th>
-          <th id="cabecalho">DESC R$</th>
-          <th id="cabecalho">INICIO</th>
-          <th id="cabecalho">FIM</th>
-          <th id="cabecalho">INSERIR</th>
-        
-          
-        </tr>
-      </thead>
-      <tbody>
+<div class="container">
+  <h2>Bordered Table</h2>
+  <p>The .table-bordered class adds borders on all sides of the table and the cells:</p>
+  <table id="example" class="table table-bordered">
+    <thead>
+    <tr>
+      <th>Mercado</th>
+      <th>Nome</th>
+      <th>Preço uni</th>
+      <th>R$ Venda</th>
+      <th>% des.</th>
+      <th>R$ Desc</th>
+      <th>Inicio</th>
+      <th>Fim</th>
+    </tr>
+    </thead>
+    <tbody>
     <?php
-    if(isset($_GET['idempresa'])){
-      $pegaid = $_GET['idempresa'];
-    
-    $sql = "SELECT * FROM produtos where idempresa = $pegaid";
+    $sql = "SELECT * FROM produtos";
     include_once("conexao.php");
     $query = mysqli_query($conn, $sql);
     while ($resultado = mysqli_fetch_assoc($query)) {
@@ -68,33 +71,73 @@
 ?>
 
 <form action="processa_oferta.php" method="GET">
-
   <tr>
-    <input type="hidden" placeholder="idempresa" value="<?php echo $idempresa; ?>" name="idempresa" readonly>
+    <td>
+
+    <select name="idempresa" id="idempresa">
+      <?php 
+
+    $sql2 = "SELECT * FROM empresas";
+    include_once("conexao.php");
+    $query2 = mysqli_query($conn, $sql2);
+    while ($resultado = mysqli_fetch_assoc($query2)) {?>
+      <option value="<?php echo $resultado['idempresa'];?>"><?php echo $resultado['nome'];?></option>
+    <?php } ?>
+    </select>
+
+    </td>
+ 
     <input type="hidden" placeholder="idproduto" value="<?php echo $id_produto; ?>" name="idproduto" readonly>
     <td class="input-nome"><input type="text" placeholder="nome produto" value="<?php echo $nome; ?>" name="nome" readonly></td>
-    <td class="valor"><input type="text" placeholder="R$ unit" value="<?php echo $preco_unitario; ?>" name="preco_unitario" readonly></td>
-    <td class="valor"><input type="text" placeholder="preco  venda" value="<?php echo $preco_venda; ?>" name="preco_venda" readonly></td>
-    <td class="valor"> <input type="text" placeholder="R$ " value="" name="preco_oferta"></td>
-    <td class="valor" ><input type="text" placeholder="Inicio" value="" name="porcentagem_desconto"></td>
-    <td class="valor"> <input type="text" placeholder="R$ " value="" name="valor_atualizado"></td>
+    <td class="ids"><input type="text" placeholder="R$ unit" value="<?php echo $preco_unitario; ?>" name="preco_unitario" readonly></td>
+    <td class="ids"><input type="text" placeholder="preco  venda" value="<?php echo $preco_venda; ?>" name="preco_venda" readonly></td>
+    <td class="ids"> <input type="text" placeholder="R$ " value="" name="preco_oferta"></td>
+    <td><input type="text" placeholder="Inicio" value="" name="porcentagem_desconto"></td>
+    <td class="ids"> <input type="text" placeholder="R$ " value="" name="valor_atualizado"></td>
     <td><input type="date" placeholder="Inicio" value="" name="inicio_oferta"></td>
     <td><input type="date" placeholder="Fim " value="" name="fim_oferta"></td>
-    
-    <td><button type="submit" class="btn btn-primary">Ofertar</button></td>
+    <td><button type="submit" class="btn btn-success">Ofertar</button></td>
   </tr>
 </form>
-<?php } } ?>
+<?php } ?>
 
 </tbody>
-    </table>
-  </div>
+</table>
 </div>
-<script>
-  $(document).ready(function () {
-    $('#example').DataTable();
-});
- </script>
 
+<script>
+$(document).ready(function() {
+  $('#example').DataTable({
+    language: {
+      "sEmptyTable": "Nenhum registro encontrado",
+      "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+      "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+      "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+      "sInfoPostFix": "",
+      "sInfoThousands": "",
+      "sLengthMenu": "Mostrar _MENU_ registros por página",
+      "sLoadingRecords": "Carregando...",
+      "sProcessing": "Processando...",
+      "sSearch": "Buscar:",
+      "sZeroRecords": "Nenhum registro encontrado",
+      "oPaginate": {
+        "sFirst": "Primeiro",
+        "sLast": "Último",
+        "sNext": "Próximo",
+        "sPrevious": "Anterior"
+      },
+      "oAria": {
+        "sSortAscending": ": Ordenar colunas de forma ascendente",
+        "sSortDescending": ": Ordenar colunas de forma descendente"
+      },
+      "buttons": {
+        "copy": "Copiar",
+        "colvis": "Visibilidade"
+      }
+    }
+  });
+});
+</script>
 </body>
 </html>
+
